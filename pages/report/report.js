5ad1dd25d794293dcb9335a7dd875a35c2eba298
jade <mayuhome@163.com>
2024-12-30 22:58:56 +0800

    feat: add header

--- pages/report/report.js
+++ pages/report/report.js
@@ -3,14 +3,25 @@ Page({
   data: {
     keyword: '',
     incomes: [],
+    totalAmount: 0,
+    currentAmount: 0
   },
 
   onLoad() {
     IncomeAPI.getIncomeList().then(res => {
+      const totalAmount = res.reduce((total, item) => total + item.amount, 0);
+      const currentAmount = res.reduce((total, item) => total + item.currentAmount, 0);
       this.setData({
-        incomes: res
+        incomes: res,
+        totalAmount,
+        currentAmount
       })
     })
+  },
+
+  handleSearch(e) {
+    console.log('e:',e);
+    this.setData({ keyword: e.detail || '' });
   },
 
   // 显示 Toast 提示
