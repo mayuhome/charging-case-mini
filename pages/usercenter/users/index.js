86b515bfcf448c6ccea2562866e83abe78b133bb
jade <mayuhome@163.com>
2025-03-11 22:35:13 +0800

    fix: user add view

--- pages/usercenter/users/index.js
+++ pages/usercenter/users/index.js
@@ -12,7 +12,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-    this.getUsers();
+    
   },
 
   /**
@@ -26,7 +26,7 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow() {
-
+    this.getUsers();
   },
 
   /**
